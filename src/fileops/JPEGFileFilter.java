a116ebbf1156f416968070083569b5ae148b9328
Alonso del Arte <alonso.delarte@gmail.com>
2022-05-28 12:16:42 -0400

    Passing more of the tests

--- src/fileops/JPEGFileFilter.java
+++ src/fileops/JPEGFileFilter.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2022 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package fileops;
 
@@ -9,15 +20,22 @@ import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
 /**
- *
+ * A filter to tell a file chooser to accept JPEG files and reject files of 
+ * other types. This filter only checks the file extension, it does not try to 
+ * validate the contents according to the JPEG standard. The filter also accepts 
+ * directories.
  * @author Alonso del Arte
  */
 public class JPEGFileFilter extends FileFilter {
 
-    // STUB TO FAIL THE FIRST TEST
+    /**
+     * Determines whether or not to accept a file.
+     * @param file The file to accept or reject.
+     * @return True if the file has the *.jpg extension, false otherwise.
+     */
     @Override
     public boolean accept(File file) {
-        return true;
+        return file.getName().endsWith(".jpg");
     }
 
     // STUB TO FAIL THE FIRST TEST
--- test/fileops/JPEGFileFilterTest.java
+++ test/fileops/JPEGFileFilterTest.java
@@ -17,11 +17,78 @@ import static org.junit.Assert.*;
 public class JPEGFileFilterTest {
     
     /**
-     * Test of accept method, of class JPEGFileFilter.
+     * Test of the accept function, of the JPEGFileFilter class. The extension 
+     * *.jpg should be accepted.
      */
     @Test
     public void testAccept() {
-        System.out.println("accept");String filename = "image.jpg";
+        System.out.println("accept");
+        String filename = "image.jpg";
+        File file = new File(filename);
+        JPEGFileFilter filter = new JPEGFileFilter();
+        String msg = "JPEG file filter should accept file " + file.toString();
+        assert filter.accept(file) : msg;
+    }
+    
+    /**
+     * Another test of the accept function, of the JPEGFileFilter class. The 
+     * extension *.png should be rejected.
+     */
+    @Test
+    public void testRejectPNG() {
+        String filename = "image.png";
+        File file = new File(filename);
+        JPEGFileFilter filter = new JPEGFileFilter();
+        String msg = "JPEG file filter should reject file " + file.toString();
+        assert !filter.accept(file) : msg;
+    }
+
+    /**
+     * Another test of the accept function, of the JPEGFileFilter class. The 
+     * extension *.txt should be rejected.
+     */
+    @Test
+    public void testRejectText() {
+        String filename = "document.txt";
+        File file = new File(filename);
+        JPEGFileFilter filter = new JPEGFileFilter();
+        String msg = "JPEG file filter should reject file " + file.toString();
+        assert !filter.accept(file) : msg;
+    }
+
+    /**
+     * Another test of the accept function, of the JPEGFileFilter class. The 
+     * extension *.html should be rejected.
+     */
+    @Test
+    public void testRejectHTML() {
+        String filename = "index.html";
+        File file = new File(filename);
+        JPEGFileFilter filter = new JPEGFileFilter();
+        String msg = "JPEG file filter should reject file " + file.toString();
+        assert !filter.accept(file) : msg;
+    }
+
+    /**
+     * Another test of the accept function, of the JPEGFileFilter class. The 
+     * extension *.rtf should be rejected.
+     */
+    @Test
+    public void testRejectRichText() {
+        String filename = "document.rtf";
+        File file = new File(filename);
+        JPEGFileFilter filter = new JPEGFileFilter();
+        String msg = "JPEG file filter should reject file " + file.toString();
+        assert !filter.accept(file) : msg;
+    }
+
+    /**
+     * Another test of the accept function, of the JPEGFileFilter class. The 
+     * extension *.JPG should be accepted.
+     */
+    @Test
+    public void testAcceptUpperCaseExtension() {
+        String filename = "image.JPG";
         File file = new File(filename);
         JPEGFileFilter filter = new JPEGFileFilter();
         String msg = "JPEG file filter should accept file " + file.toString();
@@ -29,7 +96,8 @@ public class JPEGFileFilterTest {
     }
 
     /**
-     * Test of getDescription method, of class JPEGFileFilter.
+     * Test of the getDescription function, of the JPEGFileFilter class. The 
+     * description should include the acronym "JPEG".
      */
     @Test
     public void testGetDescription() {
